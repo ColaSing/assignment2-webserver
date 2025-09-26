35471cebe2e42eebc58c5a13682a4a41d3552896
cola <csin120@wgu.edu>
2025-09-26 17:02:47 -0400

    Attempt 1

--- webServer.py
+++ webServer.py
@@ -12,30 +12,33 @@ def webServer(port=13331):
   serverSocket.bind(("", port))
   
   #Fill in start
+  serverSocket.listen(1)
 
   #Fill in end
+  print("Waiting for connection...")
 
   while True:
     #Establish the connection
-    
+
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
-    
+    conn, addr = serverSocket.accept() #Fill in start -are you accepting connections?     #Fill in end
+
     try:
-      message = #Fill in start -a client is sending you a message   #Fill in end 
+      message = conn.recv(1024).decode() #Fill in start -a client is sending you a message   #Fill in end
+
       filename = message.split()[1]
       
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:],     #fill in start              #fill in end   )
-      
+      f = open(filename[1:], 'rb' )   #fill in start             #fill in end   )
+
       
 
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
       #Fill in start 
-              
+      headerok = b"HTTP/1.1 200 OK\r\n" + b"Content-Type: text/html; charset=UTF-8\r\n" + b"Connection: close\r\n" + b"\r\n"
       #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+      #outputdata = b"Content-Type: text/html; charset=UTF-8\r\n" + b"Connection: close\r\n" + b"\r\n"
 
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
@@ -44,27 +47,33 @@ def webServer(port=13331):
                
       for i in f: #for line in file
       #Fill in start - append your html file contents #Fill in end 
-        
+        headerok += i
+      f.close()
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
+      conn.send(headerok)
+
 
       # Fill in start
 
 
       # Fill in end
         
-      connectionSocket.close() #closing the connection socket
+      conn.close() #closing the connection socket
       
     except Exception as e:
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start
+      header404 = b"HTTP/1.1 404 Not Found\r\n" + b"Content-Type: text/html; charset=UTF-8\r\n" + b"Connection: close\r\n" + b"\r\n" + b"<html><body><h1>404</h1></body></html>"
 
       #Fill in end
-
+      conn.send(header404)
 
       #Close client socket
       #Fill in start
+      conn.close()
+
 
       #Fill in end
 
